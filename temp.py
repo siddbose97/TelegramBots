c5725e4536a27919f0c2c7d6cf9cfcfe9147d7d4
Siddhartha Bose <siddbose97@gmail.com>
2021-08-11 14:55:28 +0800

    added sorting for all the AEDs, and returns 2 closest

--- temp.py
+++ temp.py
@@ -1,7 +1,7 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-
+from time import sleep
 import os
 import telebot
 import geopy.distance
@@ -21,20 +21,37 @@ class AED:
     def __init__(self, location):
         self.latitude = location.latitude
         self.longitude = location.longitude
+        self.aeds = {}
+        
        
 
 locations = {
     "nsdc": {
         1: (1.4055524, 103.8182829),
         2: (1.4067986, 103.8192924),
-        3: (1.4091958,103.8186530)
+        3: (1.4091958,103.8186530),
 
     }
 
 }
 
+badURL = "https://lh3.googleusercontent.com/m61vTtkjihiwIJ_PEnVPEKfd8Yx1FsoGcCzcrx0A5SJjZtnpu7h61HB-viz0K4JpBnK2QAf0KRfN8AA-spL_C6SIf-tMu4o3x5W6RMuG37RXP4X4COk6QRrE0ylLQJYVPLJ3-G3wQC5MQkwCcksHJfTq2UCYxKdPlSdy5pzb6m5g7Z2pot_z8_uxssfM0FjPmQ9EpprG_g_3wZ1hwp1-iEr3_RYnZUt7YzfaDDFWMlElGVCwfJ6dbzqD2NCNTGlHn_webljEdtswpUQx-2H2EHxBtlU03_ey4fKz0rGCUhN2ryyauTfdTzfvsiw6WB2Bz5uency7qEqA8EiooqZwVgRvDFOpPp8tTEZLI7JnErd_dqwrDr-3nJLdTyEntpGSlKlKBdbl4o6R1-JysFom6VcjAPW9Y1o-ZKOwoWhvVkOFHLad87PyU6_N8cBY_5WiaTbFs5Nrjl_sY84ViBp_lx4MOaW6xpjS9evotqJ2HJcen5rPGTEz3o89v2evb8nKVfqnI-M2cnTSd9qN6M8aqvz8K1XtI_ts61wVbVNCM4S1UoohWTQB0MYHkAeljkzK7unLavErhZSajtRKgfHhfIfbPK-eHHyXFxbqQ_qFCAki9FCfUT79zd80TFURjb8DlcT0dBOtyIG75D8_cCkwG2JV4_Y3M1N77K0EOfDi80m51aHxfW6znaHBob_1em6qZxBLBQadGBTQvCj_jOWEVcgOsg=w800-h399-no?authuser=1"
+campMaps = {
+    "nsdc": "https://lh3.googleusercontent.com/jz7l8taMGnOPqRTafzGq_3Uflb92HE54IkQ4AwD1Za_e2W5ywkfP-fqYV2V_aQ91z7tKvBAbpGV2w9F5uB6zNXp2YOEuaXV3cOnA5WSQeiAmE-cMCW-K4GMSjSEozIqZ2pxt2Nz1aF_dWwHhczezH3nUWo1cK85IFD8HPd4Ct1qq-YlP84q0MN9JQAKTtnsowsuPe8IiXyq33UuHhzluOkkc2_pQdBBWZxgA7Sit767DkZO30GBrGSNkWI9GPk2G6PEWM5edoRgWqtxaFKXeYsPV4kd9eCud4SykFGlRHJuxk5c_CXA2FSZH7ZFQk1ikTyBkvMgAY9UycxNPO8lUxFb6FLwLqKBKoXcP7uIG0BfFtT9YLVh135NSuRBvl538qQG-zhkv1tYi-w03LgRdo9zk5790QrEszkkKFL8fcGoqkmuGrtIzGQ5mEgXPqhh4hZoiSkaDTMZHDLgTBl6IitaIrWtmnzs9f8OFNQGSGuB_4qAFavOgCvKITX4WiimtWQaD7WGky8RVKFiB7dlLc2uhu_BUeVVtUaD2EzWelye0lKr1dpob7LK6qd30Br2qf_7LDVUzjbMbRjE5LDn1UWsYXAT8oKWRn213sEwArF56lUDb8kYdpIkHQApqyjDuykVjPRrRRhXtNTjtOJdP4ZdIqZ15ks9eqU1F0P2dcTiZbWSqAO-esahf0zXS7I1XTFZiH6wXX6Plw2PGG9eGmoK0zw=w596-h792-no?authuser=1",
+    "nsc": badURL,
+    "mandai hill":badURL,
+    "kc2":badURL,
+    "kc3" :badURL,
+    "mowbray": badURL,
+    "hendon":badURL,
+    "clementi":badURL,
+    "maju":badURL,
+    "gombak":badURL,
+    "gedong":badURL
+}
 
-
+mappingMapDict = {1:"nsdc", 2:"nsc", 3:"mandai hill", 4:"kc2", 5:"kc3", \
+    6:"mowbray", 7: "hendon", 8:"clementi", 9:"maju", 10:"gombak", 11:"gedong"}
 
 
 
@@ -83,21 +100,77 @@ def currentLocation(message):
             aed = AED(message.location)
             aedDict[chat_id] = aed
             print(message.location)
+            sendString = """
+            Which camp are you at?
+
+            Options are (please input number only):
+            1. nsdc
+            2. nsc
+            3. mandai hill
+            4. kc2
+            5. kc3
+            6. mowbray
+            7. hendon
+            8. clementi
+            9. maju
+            10. gombak
+            11. gedong
+        """
+            
+            msg = bot.send_message(message.chat.id,sendString )
+            bot.register_next_step_handler(msg, distanceCalculator)
+        else:
+            bot.send_message(message.chat.id,"Could not get user location, please try again" )
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+
+def distanceCalculator(message):
+    try:
+        chat_id = message.chat.id
+        camp =  int(message.text)
+        aed = aedDict[chat_id]
+        
+        if mappingMapDict[camp] in locations.keys():
             closestAED = (0,0)
             minDist = 100000000000
-            for coords in locations["nsdc"].values():
-                #dist = geopy.distance.distance((aed.latitude, aed.longitude), coords).m
-                dist = geopy.distance.distance((1.408879, 103.818540), coords).m
-                print(dist)
+            for coords in locations[mappingMapDict[camp]].values():
+                dist = geopy.distance.distance((aed.latitude, aed.longitude), coords).m
+                aed.aeds[dist] = coords
+                #dist = geopy.distance.distance((1.408879, 103.818540), coords).m
+                
                 if dist < minDist:
                     minDist = dist
                     closestAED = coords
+            sortedDist = sorted(list(aed.aeds.keys()))
+            print(sortedDist)
+            bot.send_message(message.chat.id,"The AEDs below are sorted from nearest to farthest!" )
+            bot.send_chat_action(message.chat.id, "typing")
+            sleep(3)
+            counter = 0
+            for keys in sortedDist:
+                if counter > 1:
+                    break
+                bot.send_location(message.chat.id, aed.aeds[keys][0], aed.aeds[keys][1])
+                sendString = "The AED at the above location is approximately " + str(round(keys)) + "m away"
+                bot.send_message(message.chat.id,sendString )
+                counter += 1
+                
+                sleep(0.5)
+            finalString = "Stay Safe!"
+            bot.send_message(message.chat.id, finalString )
 
-            bot.send_location(message.chat.id, closestAED[0], closestAED[1])
-            sendString = "The closest AED is at the above location, approximately " + str(round(minDist)) + "m away"
-            bot.send_message(message.chat.id,sendString )
+            # bot.send_location(message.chat.id, closestAED[0], closestAED[1])
+            # sendString = "The closest AED is at the above location, approximately " + str(round(minDist)) + "m away"
+            # bot.send_message(message.chat.id,sendString )
+        else:
+            bot.send_message(message.chat.id,"Sorry, we currently don't have the coordinates of the AEDs in this camp!" )
     except Exception as e:
         bot.reply_to(message, 'oooops')
+
+
+
+#========================================================================
 
 
 def checker (message):
@@ -107,24 +180,24 @@ def checker (message):
         return False
 
 
-
 @bot.message_handler(func=checker)
 def staticMap(message):
     try:
         msg = bot.reply_to(message, """\
         Which camp would you like a map for?
 
-        Options are:
+        Options are (please input number only):
         1. nsdc
-        2. kc2
-        3. kc3
-        4. hendon
-        5. mowbray
-        6. clementi
-        7. maju
-        8. mandai hill
-        9. gombak
-        10. gedong
+        2. nsc
+        3. mandai hill
+        4. kc2
+        5. kc3
+        6. mowbray
+        7. hendon
+        8. clementi
+        9. maju
+        10. gombak
+        11. gedong
         """)
         bot.register_next_step_handler(msg, returnImage)
     except Exception as e:
@@ -132,10 +205,13 @@ def staticMap(message):
 
 def returnImage(message):
     try:
-       
-        url = "https://lh3.googleusercontent.com/-78QT2XudDyQlv84s3sJRs4e2A2CWoXLTX9n6sUM9Wc8SU6JO07PjMuvjhF41Iz47vEhI0OjQxtv6kM8X85pRl92xCPz3w9Faws-hEqSCtnT7DoCpJD1PosHvohVXItHoI4a53V0mgaMaI0t9Pj69hYO1NP5edwNLWbNw19ij4B8B0KR329nXgJEqt0hhdNKoPEemcjBsrpNNINMByGxDFGFmcZk8uLw0bspdiIuHrgDgGTfCFKGjA_DS7NJm_tc9j86cyFk-BsJncUpcPpjVJXmlikJGltQRxRJpljrlfxcK8svzIRlVYRYSXavzmkesyF80bjmLsYYD4Ht5O1dVbnc-JNCZPIL3c1f0EC9_ccZNf9K9DVjYvVVabbeUsobGMuf2y9bW0LePusAxXssPeXwcMuFO9UU7q-lGoRSaWrKxwfHWCvDG5b6YQ4RllE72iwT-eDrmlTG-RzV3vT9JSG3g_XOgY4XMJDCzUMMnH-Jh2duaqdBuQxyj7_iav9SEhPPjCtVSvVnJtDefsteeplOlziQleooo89pSML1mWz5tBUKcEtX8WRfA4cJeT6joam4a4BTi2BpaDDqNzisyc7RfbHu7y5o9TICNk-GnYxw873-FqT1DBN_tErjlOGfG4WFRw_hM_mrQnfJZndg98-H40s5XpigQAwIiECdLHudEdkeGcEDDmpOwRSIN16acUzNvYP7anwn8tfY7-eCh534WQ=w828-h775-no?authuser=1"
         chat_id = message.chat.id
+        msg = message.text
+        url = campMaps[mappingMapDict[int(msg)]]
         bot.send_photo(chat_id=chat_id, photo=url)
+        if url == badURL:
+            errorString = "Sorry, support for this camp is not available yet!"
+            bot.send_message(message.chat.id,errorString )
     except Exception as e:
         bot.reply_to(message, 'oooops')
 
